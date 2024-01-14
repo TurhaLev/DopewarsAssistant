4578f4fac53c32e64bd6926d4068333ee831b1b0
TurhaLev <turha@rambler.ru>
2024-01-14 14:40:10 +0300

    Начальная инициализация репозитория

---
+++ program/dwa_ass.py
@@ -0,0 +1,31 @@
+"""
+    Основной файл программы помощника Dopewars
+"""
+
+from dataclasses import dataclass
+from dwa_consts import ALL_DRAGS, ALL_LOCATIONS, GUNS, WINDOW_SIZE
+
+
+@dataclass
+class Player:
+    """Описание данных игрока"""
+
+    drugs: dict  # наименование и количество товара в наличии
+    guns: dict  # наименование и количество оружия
+    place: str  # текущее местонахождение
+    cash: int  # количество наличных денег при себе
+    deposit: int  # количество денег на депозите в банке
+    space: int  # полное количество слотов
+
+    def __init__(self):
+        self.drugs = {}
+        for item in ALL_DRAGS:
+            drug = {"quantity": 0, "price": 0}
+            self.drugs.update({item: drug})
+        self.place = ALL_LOCATIONS[0]
+        self.cash = 2000
+        self.deposit = 0
+        self.space = 100
+        self.guns = {}
+        for gun in GUNS:
+            self.guns.update({gun: 0})
---
+++ program/dwa_consts.py
@@ -0,0 +1,38 @@
+"""Определения для всей программы
+"""
+
+ALL_DRAGS = (
+    "Acid",
+    "Cocaine",
+    "Hashish",
+    "Heroin",
+    "Ludes",
+    "MDA",
+    "Opium",
+    "PCP",
+    "Peyote",
+    "Shrooms",
+    "Speed",
+    "Viagra",
+    "Weed",
+)
+
+ALL_LOCATIONS = (
+    "Bronx",
+    "Ghetto",
+    "Central Park",
+    "Manhattan",
+    "Coney Island",
+    "Brooklyn",
+    "Queens",
+    "Staten Island",
+)
+
+GUNS = {
+    "Baretta": 3000,
+    ".38 Special": 3500,
+    "Ruger": 2900,
+    "Saturday Night Special": 3100,
+}
+
+WINDOW_SIZE = (600, 400)
